721f02b7c262524494e2871a18087c6f7188c5fd
Niklas Winde <niklas.winde@rail.bombardier.com>
2018-03-13 07:24:18 +0100

    Some refactoring
    Prepared for binary search during iteration. Some commenting

--- NSort.py
+++ NSort.py
@@ -5,39 +5,45 @@ import random
 
 random.seed()
 
-
+# Iterates a number of times for test purposes
 for j in range(10):
     toBeSorted = []
     sortedList = []
 
-    operations = 1
+    operations = 2 # First two operations happen outside loop
 
+    # Generate random test list
     for i in range(1000):
         toBeSorted.append(random.randint(0, 100))
 
-    while len(toBeSorted) > 0:
+    # No items yet in sorted list
+    item = toBeSorted.pop()
+    sortedList.append(item)
+    
+    # Second item to sorted list
+    item = toBeSorted.pop()
+    # Items fits outside sorted list (it always does for second item)
+    if (item <= sortedList[0]):
+        sortedList.insert(0, item)
+    elif (item >= sortedList[-1]):
+        sortedList.append(item)
+    
+    # Actually sort. TODO: Refactor
+    # First 20 position is naïve regular iteration if items are inside sorted list
+    for i in range(20):
         item = toBeSorted.pop()
-
-        # No items yet in sorted list
-        if (len(sortedList) == 0):
-            sortedList.append(item)
-            #print ("added first")
-            continue
 
         # Items fits outside sorted list
         if (item <= sortedList[0]):
             sortedList.insert(0, item)
-            #print ("inserting")
             operations += 1
             continue
         elif (item >= sortedList[-1]):
             sortedList.append(item)
-            #print ("appending")
             operations += 1
             continue
 
         # Must iterate to find place in array
-        #print ("iterating")
         found = False
         index = 0
         while (found == False):
@@ -45,9 +51,32 @@ for j in range(10):
             if ((item > sortedList[index]) and (item <= sortedList[index + 1])):
                 sortedList.insert(index + 1, item)
                 found = True
-                #print ("iterated")
             else:
                 index += 1
 
+    # Rest of items use binary search in iterations
+    while len(toBeSorted) > 0:
+        item = toBeSorted.pop()
+
+        # Items fits outside sorted list
+        if (item <= sortedList[0]):
+            sortedList.insert(0, item)
+            operations += 1
+            continue
+        elif (item >= sortedList[-1]):
+            sortedList.append(item)
+            operations += 1
+            continue
+
+        # Must iterate to find place in array
+        found = False
+        index = 0
+        while (found == False):
+            operations += 1
+            if ((item > sortedList[index]) and (item <= sortedList[index + 1])):
+                sortedList.insert(index + 1, item)
+                found = True
+            else:
+                index += 1
         
     print ("operations: " + str(operations))
